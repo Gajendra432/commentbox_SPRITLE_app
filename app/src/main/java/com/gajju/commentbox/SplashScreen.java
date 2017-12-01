74159a522a522ec00ebaf0a29fae71d2c88eff52
gajendragajju <gk797532@gmail.com>
2017-12-01 07:40:55 +0530

    Initial commit

---
+++ app/src/main/java/com/gajju/commentbox/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.gajju.commentbox;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by gajju on .
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent openMainActivity= new Intent("com.gajju.commentbox.MAINACTIVITY");
+                    startActivity(openMainActivity);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
